b5f9892ddf9ddf718d7c37c4d2f270d3c3c63edd
kh-almas <services.almas@gmail.com>
2024-02-01 18:20:56 +0600

    update and delete api

--- src/routes/inventory-management/stock-adjustment/stock.adjustment.route.js
+++ src/routes/inventory-management/stock-adjustment/stock.adjustment.route.js
@@ -1,12 +1,8 @@
 // require
 const express = require("express");
 const userVerify = require("../../../middlewares/auth/user.verify");
-const paramsValidation = require("../../../validations/shared/params.validation");
-const { addModel, getModel, updateModel, deleteModel } = require('../../../controllers/inventory-management/product/model/model.controller');
-const modelValidation = require("../../../validations/inventory-management/product/model/model.validation");
 const stockAdjustmentValidation = require("../../../validations/inventory-management/inventory/stock.adjustment.validation");
 const { addStockAdjustment, getAllStockAdjustment, updateStockAdjustment, deleteStockAdjustment} = require("../../../controllers/inventory-management/inventory/stock-adjustment/stock.adjustment.controller");
-
 
 // router
 const stockAdjustmentRouter = express.Router();
