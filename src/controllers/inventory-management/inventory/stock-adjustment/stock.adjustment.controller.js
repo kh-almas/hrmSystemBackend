2525cbbea058fb2a2e1cd4286449ef4e2dc9fafd
Iambijoy656 <bijoy.chandrodas56@gmail.com>
2024-02-01 16:05:05 +0600

    adjust ment

---
+++ src/controllers/inventory-management/inventory/stock-adjustment/stock.adjustment.controller.js
@@ -0,0 +1,216 @@
+// require
+const getDatabaseConnection = require("../../../../configs/db.config");
+
+const addStockAdjustment = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    // // FROM hrm_branch;
+    const {branch_id, sku_id, batch_no, date, qty, purchase_price, selling_price, total_discount} = req.body
+    // const obj = {branch_id, sku_id, batch_no, date, qty, purchase_price, selling_price, total_discount};
+    // obj.created_by = req.decoded.id;
+    // obj.updated_by = req.decoded.id;
+
+    // const pricingObj = {branch_id, sku_id, batch_no, date, purchase_price, selling_price};
+    // pricingObj.created_by = req.decoded.id;
+    // pricingObj.updated_by = req.decoded.id;
+
+    // const connection = await getDatabaseConnection();
+    // const [checkUnique] = await connection.query(
+    //     `SELECT LPAD(FN_primary_id_opening_stock (${branch_id}, 2), 18, '0') AS Result`
+    // );
+    // obj.primary_id = checkUnique?.[0]?.Result;
+    // pricingObj.primary_id = checkUnique?.[0]?.Result;
+
+    // console.log('checkUnique', checkUnique)
+    // const [stockRow] = await connection.query(
+    //     "INSERT INTO inventory_opening_stock SET ?",
+    //     obj
+    // );
+    // const [PricingRow] = await connection.query(
+    //     "INSERT INTO inventory_product_pricing SET ?",
+    //     pricingObj
+    // );
+    // connection.release();
+
+    return res.status(200).json({
+        status: "ok",
+        body: {
+            message: "one opening added",
+            brand: 'row',
+        },
+    });
+  } catch (err) {
+    console.error(`add opening stock error: ${err}`);
+
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot add opening stock" },
+    });
+  }
+};
+
+const getAllStockAdjustment = async (req, res) => {
+  try {
+    const connection = await getDatabaseConnection();
+    const [row] = await connection.query(
+      `SELECT 
+            ios.id as id,
+            ios.branch_id as branch_id,
+            ios.sku_id as sku_id,
+            ios.date as date_s_g,
+            branch.name as name_s,
+            product.name as product_s,
+            ios.batch_no as batch_s,
+            ios.qty  as quantity_s,
+            ios.purchase_price as purchase_price_s,
+            ios.selling_price as selling_price_s,
+            ios.total_discount as total_discount_s
+             FROM  inventory_opening_stock AS ios
+             LEFT JOIN hrm_branch AS branch ON ios.branch_id = branch.id
+             LEFT JOIN inventory_products_sku AS sku ON ios.sku_id = sku.id
+             LEFT JOIN inventory_products AS product ON sku.product_id = product.id`
+    );
+    connection.release();
+
+    // if (!row.length) throw "no opening stock found";
+
+    return res.status(200).json({
+      status: "ok",
+      body: {
+        message: "get all opening stock`",
+        data: row,
+      },
+    });
+  } catch (err) {
+    console.error(`get opening stock error: ${err}`);
+
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot get opening stock" },
+    });
+  }
+};
+
+const updateStockAdjustment = async (req, res) => {
+  try {
+    const { batchNo } = req.params;
+    console.log(batchNo, "body");
+    const {
+      batch_no,
+      qty,
+      purchase_price,
+      selling_price,
+      total_discount,
+      branch_id,
+      sku_id,
+      date,
+    } = req.body;
+
+    const pricingObj = {
+      branch_id,
+      sku_id,
+      batch_no,
+      date,
+      purchase_price,
+      selling_price,
+    };
+    pricingObj.created_by = req.decoded.id;
+    pricingObj.updated_by = req.decoded.id;
+
+    const obj = {
+      branch_id,
+      sku_id,
+      batch_no,
+      date,
+      qty,
+      purchase_price,
+      selling_price,
+      total_discount,
+    };
+    obj.created_by = req.decoded.id;
+    obj.updated_by = req.decoded.id;
+
+    const connection = await getDatabaseConnection();
+    // const [checkUnique] = await connection.query(
+    //     `SELECT LPAD(FN_primary_id_opening_stock (${branch_id}, 2), 18, '0') AS Result`
+    // );
+    // obj.primary_id = checkUnique?.[0]?.Result;
+    // pricingObj.primary_id = checkUnique?.[0]?.Result;
+
+    // console.log('checkUnique', checkUnique)
+    const [stockRow] = await connection.query(
+      "UPDATE inventory_opening_stock SET ? WHERE batch_no = ?",
+      [obj, batchNo]
+    );
+    const [PricingRow] = await connection.query(
+      "UPDATE inventory_product_pricing SET ? WHERE batch_no = ?",
+      [pricingObj, batchNo]
+    );
+    connection.release();
+
+    return res.status(200).json({
+      status: "ok",
+      body: {
+        message: "one opening added",
+        brand: "row",
+      },
+    });
+
+    // const brand = ({ name, description, status, pc_address } = req.body);
+    // brand.created_by = req.decoded.email;
+    // brand.updated_by = req.decoded.email;
+    //
+    // const connection = await getDatabaseConnection();
+    // const [row] = await connection.query(
+    //     "UPDATE inventory_product_brands SET ? WHERE id = ?",
+    //     [brand, id]
+    // );
+    // connection.release();
+    //
+    // return res.status(200).json({
+    //     status: "ok",
+    //     body: { message: "one brand updated", contact: row },
+    // });
+  } catch (err) {
+    console.error(`add brand error: ${err}`);
+
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot update brand" },
+    });
+  }
+};
+
+const deleteStockAdjustment = async (req, res) => {
+  try {
+    const { batchNo } = req.params;
+
+    const connection = await getDatabaseConnection();
+    const [openingStock] = await connection.query(
+      "DELETE FROM inventory_opening_stock WHERE batch_no = ?",
+      [batchNo]
+    );
+    const [pricing] = await connection.query(
+      "DELETE FROM inventory_product_pricing WHERE batch_no = ?",
+      [batchNo]
+    );
+    connection.release();
+
+    return res.status(200).json({
+      status: "ok",
+      body: { message: "one brand deleted", contact: "row" },
+    });
+  } catch (err) {
+    console.error(`delete brand error: ${err}`);
+
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot delete brand" },
+    });
+  }
+};
+
+module.exports = {
+  addStockAdjustment,
+};
---
+++ src/validations/inventory-management/inventory/stock.adjustment.validation.js
@@ -0,0 +1,41 @@
+
+// require
+const Joi = require("joi");
+
+// schema
+const ValidationSchema = Joi.object({
+
+    branch_id: Joi.number().required(),
+    sku_id: Joi.number().required(),
+    batch_no: Joi.string().required(),
+    purpose: Joi.string().required(),
+    ref_id: Joi.string().required(),
+    date: Joi.date().required(),
+    qty: Joi.number().required(),
+    purchase_price: Joi.number().required(),
+    selling_price: Joi.number().required(),
+    total_discount: Joi.number().required(),
+});
+
+
+// add model validation
+const stockAdjustmentValidation = async (req, res, next) => {
+    try {
+        const {branch_id, sku_id, batch_no, date, qty, purchase_price, selling_price, total_discount} = req.body
+        const obj = {branch_id, sku_id, batch_no, purpose , ref_id ,date, qty, purchase_price, selling_price, total_discount};
+
+        await ValidationSchema.validateAsync(obj);
+
+        next();
+    } catch (err) {
+        console.error(`validation error: ${err}`);
+
+        return res.status(400).json({
+            status: "error",
+            body: { message: err },
+        });
+    }
+};
+
+// export
+module.exports = stockAdjustmentValidation;
