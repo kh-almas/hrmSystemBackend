9a84b5a96e17809c70d02cb25913e1bc52afe723
FahimCC <ffnasif@gmail.com>
2024-02-20 17:26:32 +0600

    update product initialization delete method

--- src/controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller.js
+++ src/controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller.js
@@ -2,102 +2,102 @@
 const getDatabaseConnection = require("../../../../configs/db.config");
 
 const getBrandProductInitialization = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (id)
-        {
-            const connection = await getDatabaseConnection();
-            const [row] = await connection.query(
-                `SELECT product_id FROM inventory_product_inetialization WHERE branch_id = ${id}`
-            );
-            connection.release();
-            return res.status(200).json({
-                status: "ok",
-                body: {
-                    message: "get all product settings`",
-                    data: row,
-                },
-            });
-        }else{
-            return res.status(200).json({
-                status: "ok",
-                body: {
-                    message: "get all product settings`",
-                    data: 'row',
-                },
-            });
-        }
-
-
-        // if (!row.length) throw "no units found";
-
-
-    } catch (err) {
-        console.error(`get product settings error: ${err}`);
-
-        return res.status(500).json({
-            status: "error",
-            body: {
-                message: err || "cannot get settings",
-            },
-        });
+    if (id) {
+      const connection = await getDatabaseConnection();
+      const [row] = await connection.query(
+        `SELECT product_id FROM inventory_product_inetialization WHERE branch_id = ${id}`
+      );
+      connection.release();
+      return res.status(200).json({
+        status: "ok",
+        body: {
+          message: "get all product settings`",
+          data: row,
+        },
+      });
+    } else {
+      return res.status(200).json({
+        status: "ok",
+        body: {
+          message: "get all product settings`",
+          data: "row",
+        },
+      });
     }
+
+    // if (!row.length) throw "no units found";
+  } catch (err) {
+    console.error(`get product settings error: ${err}`);
+
+    return res.status(500).json({
+      status: "error",
+      body: {
+        message: err || "cannot get settings",
+      },
+    });
+  }
 };
 
 const updateBrandProductInitialization = async (req, res) => {
-    try {
-        const data  = req.body;
-        const { id } = req.params;
+  try {
+    const data = req.body;
+    const { id } = req.params;
 
-        const connection = await getDatabaseConnection();
-        data?.map(async singleData => {
-            const makeData = {branch_id: id, product_id: singleData?.id}
-            const [row] = await connection.query(
-                "INSERT INTO inventory_product_inetialization SET ?",
-                [makeData]
-            );
-        })
+    const connection = await getDatabaseConnection();
+    data?.map(async (singleData) => {
+      const makeData = { branch_id: id, product_id: singleData?.id };
+      const [row] = await connection.query(
+        "INSERT INTO inventory_product_inetialization SET ?",
+        [makeData]
+      );
+    });
 
-        connection.release();
+    connection.release();
 
-        return res.status(200).json({
-            status: "ok",
-            body: { message: "one unit updated", contact: 'row' },
-        });
-    } catch (err) {
-        console.error(`add unit error: ${err}`);
+    return res.status(200).json({
+      status: "ok",
+      body: { message: "one unit updated", contact: "row" },
+    });
+  } catch (err) {
+    console.error(`add unit error: ${err}`);
 
-        return res.status(500).json({
-            status: "error",
-            body: { message: err || "cannot update unit" },
-        });
-    }
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot update unit" },
+    });
+  }
 };
 
 const deleteBrandProductInitialization = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const connection = await getDatabaseConnection();
-        const [row] = await connection.query(
-            "DELETE FROM inventory_product_inetialization WHERE id = ?",
-            [id]
-        );
-        connection.release();
+    const connection = await getDatabaseConnection();
+    const [row] = await connection.query(
+      "DELETE FROM inventory_product_inetialization WHERE product_id = ?",
+      [id]
+    );
+    connection.release();
 
-        return res.status(200).json({
-            status: "ok",
-            body: { message: "one brand deleted", contact: row },
-        });
-    } catch (err) {
-        console.error(`delete brand error: ${err}`);
+    return res.status(200).json({
+      status: "ok",
+      body: { message: "one variant deleted", contact: row },
+    });
+  } catch (err) {
+    console.error(`delete variant error: ${err}`);
 
-        return res.status(500).json({
-            status: "error",
-            body: { message: err || "cannot delete brand" },
-        });
-    }
+    return res.status(500).json({
+      status: "error",
+      body: { message: err || "cannot delete variant" },
+    });
+  }
 };
 
-module.exports = { getBrandProductInitialization, updateBrandProductInitialization, deleteBrandProductInitialization };
+module.exports = {
+  getBrandProductInitialization,
+  updateBrandProductInitialization,
+  deleteBrandProductInitialization,
+};
