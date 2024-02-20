7ddfde1a704359f3405200e5e8f5e1f9725c2bf9
kh-almas <services.almas@gmail.com>
2024-02-20 13:40:26 +0600

    add api product initialization

--- src/controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller.js
+++ src/controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller.js
@@ -77,4 +77,29 @@ const updateBrandProductInitialization = async (req, res) => {
     }
 };
 
-module.exports = { getBrandProductInitialization, updateBrandProductInitialization };
+const deleteBrandProductInitialization = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const connection = await getDatabaseConnection();
+        const [row] = await connection.query(
+            "DELETE FROM inventory_product_inetialization WHERE id = ?",
+            [id]
+        );
+        connection.release();
+
+        return res.status(200).json({
+            status: "ok",
+            body: { message: "one brand deleted", contact: row },
+        });
+    } catch (err) {
+        console.error(`delete brand error: ${err}`);
+
+        return res.status(500).json({
+            status: "error",
+            body: { message: err || "cannot delete brand" },
+        });
+    }
+};
+
+module.exports = { getBrandProductInitialization, updateBrandProductInitialization, deleteBrandProductInitialization };
--- src/routes/inventory-management/branch-product-initialization/brand.product.initialization.route.js
+++ src/routes/inventory-management/branch-product-initialization/brand.product.initialization.route.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const userVerify = require("../../../middlewares/auth/user.verify");
 const paramsValidation = require("../../../validations/shared/params.validation");
-const {getBrandProductInitialization, updateBrandProductInitialization} = require("../../../controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller");
+const {getBrandProductInitialization, updateBrandProductInitialization, deleteBrandProductInitialization} = require("../../../controllers/inventory-management/product/brand-product-initialization/brand.product.initialization.controller");
 
 // router
 const brandProductInitializationRouter = express.Router();
@@ -15,6 +15,9 @@ brandProductInitializationRouter.get("/:id", getBrandProductInitialization);
 
 // update
 brandProductInitializationRouter.put("/update/:id", updateBrandProductInitialization);
+
+// delete
+brandProductInitializationRouter.put("/delete/:id", deleteBrandProductInitialization);
 
 // export
 module.exports = brandProductInitializationRouter;
